74edc6d1566799581623493db0f185e592a4d3c0
Brendan Raykoff <99614905+Braykoff@users.noreply.github.com>
2026-05-17 18:55:54 -0500

    Index page, link paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
+
+// Get git repo info (when run in GitHub Actions)
+const MAIN_REPO = "dailycal-projects/dailycalprojects";
+const MAIN_SITE = "https://data.dailycal.org";
+
+const repository = process.env.GITHUB_REPOSITORY ?? MAIN_REPO;
+const [owner, name] = repository.split("/");
+const isFork = repository !== MAIN_REPO;
+
+console.log(`Building for repo ${repository} (fork: ${isFork})`)
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://data.dailycal.org",
-  base: "/",
+  site: isFork? `https://${owner}.github.io` : MAIN_SITE,
+  base: isFork ? `/${name}` : "/",
+
   redirects: {
     
   },
+
   devToolbar: {
     enabled: false
-  }
+  },
+
+  integrations: [react(), mdx()]
 });
---
+++ src/components/Link.jsx
@@ -0,0 +1,58 @@
+/* 
+This allows links to other pages to be properly generated, even when the base configuration in
+astro.config.mjs is not set to '/'.
+*/
+
+const EXTERNAL_OR_SPECIAL_REGEX =
+  /^(?:[a-z][a-z\d+\-.]*:)?\/\/|^(#|mailto:|tel:)/i;
+
+/**
+ * Joins the provided path with the base path configured in astro.config.mjs.
+ * 
+ * Eg, if the path is configured to /test, and you pass in /xyz/123, the output will be /test/xyz/123.
+ * @param {*} path The path to format.
+ * @returns The formatted path
+ */
+export function joinWithBase(path) {
+  const base = import.meta.env.BASE_URL.replace(/\/$/, "");
+  path = path.replace(/^\//, "");
+
+  return `${base}/${path}`;
+}
+
+/**
+ * Checks if a URL needs to be formatted with the base path configured in astro.config.mjs, and doing
+ * so if it is. URLs which specify a protocol (http://, https://, //, etc), are special (mailto:, 
+ * tel:, #) will remain the same.
+ * @param {*} url The URL to format
+ * @returns The formatted URL.
+ */
+export function formatURL(url) {
+  // Ignore special links
+  if (EXTERNAL_OR_SPECIAL_REGEX.test(url)) {
+    return url;
+  }
+
+  // Remove leading /, add base
+  return joinWithBase(url);
+}
+
+/**
+ * A wrapper of the HTML <a> tag that formats paths to have the base specified in astro.config.mjs.
+ * Relative paths will have that base appended to the beginning.
+ * 
+ * Specify the destination with href as you would in HTML, but other properties (such as className)
+ * will have to use the React naming.
+ * @param {*} props 
+ * @returns 
+ */
+export function Link(props) {
+  const { href, ...rest } = props;
+  const formattedHref = formatURL(href);
+
+  return (
+    <a href={formattedHref} {...rest}>
+      {props.children}
+    </a>
+  )
+}
